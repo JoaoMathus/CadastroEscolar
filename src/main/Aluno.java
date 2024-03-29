446e44bca775c8178fa918af61448e50e911ac38
Desisto17 <breno_serra10@hotmail.com>
2024-03-29 16:43:01 -0300

    Feita classe Aluno e seus metodos, adicionada a classe Pessoa.

--- src/main/Aluno.java
+++ src/main/Aluno.java
@@ -1,4 +1,28 @@
 package main;
+import java.util.*;
 
-public class Aluno {
+public class Aluno extends Pessoa{
+
+    final String matricula;
+    Map<String, Float> notasDeProva = new HashMap<>();
+
+    public Aluno(String nome, String telefone, Date dataNascimento, String email, String matricula){
+        super(nome, telefone, dataNascimento, email);
+        this.matricula = matricula;
+        notasDeProva.put("AV1", 0.0f);
+        notasDeProva.put("AV2", 0.0f);
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Map<String, Float> getNotas() {
+        return notasDeProva;
+    }
+
+    public void setNota(String prova, float nota) {
+        notasDeProva.put(prova, nota);
+    }
+
 }

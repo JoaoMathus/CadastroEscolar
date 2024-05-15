528f95954710bfb0d7a4ef5b9052aec5b3809129
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-15 16:24:25 -0300

    Pacote telas criado. Ficará responsável por armazenar as telas. Por enquanto possui apenas a classe Main, que possui o ponto de entrada da aplicação.

---
+++ src/telas/Main.java
@@ -0,0 +1,9 @@
+package telas;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Funcionei!");
+    }
+}

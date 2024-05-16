ab90045c1a007271641c6ce12246681bf7f4f00f
Desisto17 <breno_serra10@hotmail.com>
2024-05-16 20:08:11 -0300

    Refeita Classe Aluno com base nas regras do negócio.

--- src/main/java/classes_base/Aluno.java
+++ src/main/java/classes_base/Aluno.java
@@ -7,23 +7,65 @@ public class Aluno extends Pessoa {
     final String matricula;
     Map<String, Float> notasDeProva = new HashMap<>();
 
-    public Aluno(String nome, String telefone, Date dataNascimento, String email, String matricula){
+    private String serie;
+    private String numeroTurma;
+    private String tipoSanguineo;
+    private List<String> telefones = new ArrayList<String>();
+    private String cpfDoResponsavel;
+
+    public Aluno(Long id, String nome, String dataNascimento, String matricula, String serie, String numeroTurma, String tipoSanguineo, List<String> telefones, String cpfDoResponsavel) {
+        super(id, nome, dataNascimento);
         this.matricula = matricula;
+        this.serie = serie;
+        this.numeroTurma = numeroTurma;
+        this.tipoSanguineo = tipoSanguineo;
+        this.telefones = telefones;
+        this.cpfDoResponsavel = cpfDoResponsavel;
         notasDeProva.put("AV1", 0.0f);
         notasDeProva.put("AV2", 0.0f);
     }
 
-    public String getMatricula() {
-        return matricula;
-    }
+    public Long getId() { return id; }
+
+    public String getNome() { return nome; }
+
+    public String getMatricula() { return matricula; }
 
     public Map<String, Float> getNotas() {
         return notasDeProva;
     }
 
+    public String getSerie() { return serie; }
+
+    public String getDataNascimento() { return dataNascimento; }
+
+    public String getNumeroTurma() { return numeroTurma; }
+
+    public String getTipoSanguineo() { return tipoSanguineo; }
+
+    public List<String> getTelefones() { return telefones; }
+
+    public String getCpfDoResponsavel() { return cpfDoResponsavel; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
     public void setNota(String prova, float nota) {
         notasDeProva.put(prova, nota);
     }
+
+    public void setSerie(String serie) { this.serie = serie; }
+
+    public void setDataNascimento(String dataNascimento) { this.dataNascimento = dataNascimento; }
+
+    public void setNumeroTurma(String numeroTurma) { this.numeroTurma = numeroTurma; }
+
+    public void setTipoSanguineo(String tipoSanguineo) { this.tipoSanguineo = tipoSanguineo; }
+
+    public void adicionarTelefone(String telefone) { telefones.add(telefone); }
+
+    public void excluirTelefone(String telefone) { telefones.remove(telefone); }
+
+    public void setCpfDoResponsavel(String cpfDoResponsavel) { this.cpfDoResponsavel = cpfDoResponsavel; }
 
     public float calcularMedia() {
         // A maravilha que é programação funcional

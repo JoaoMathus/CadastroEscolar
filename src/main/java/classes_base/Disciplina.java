a487cefb49e83c304b2ad1a21e80accc9259e15a
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-20 13:47:35 -0300

    Classe Disciplina modificada.

--- src/main/java/classes_base/Disciplina.java
+++ src/main/java/classes_base/Disciplina.java
@@ -3,13 +3,19 @@ package classes_base;
 public class Disciplina {
     private int id;
     private String nome;
+    private String bimestre;
+    private String anoLetivo;
+    private boolean aprovado;
     private float av1;
     private float av2;
     private int idAluno;
 
-    public Disciplina(int id, String nome, float av1, float av2, int idAluno) {
+    public Disciplina(int id, String nome, String bimestre, String anoLevivo,
+                      float av1, float av2, int idAluno) {
         this.id = id;
         this.nome = nome;
+        this.bimestre = bimestre;
+        this.anoLetivo = anoLevivo;
         this.av1 = av1;
         this.av2 = av2;
         this.idAluno = idAluno;
@@ -55,5 +61,29 @@ public class Disciplina {
 
     public void setIdAluno(int idAluno) {
         this.idAluno = idAluno;
+    }
+
+    public String getBimestre() {
+        return bimestre;
+    }
+
+    public void setBimestre(String bimestre) {
+        this.bimestre = bimestre;
+    }
+
+    public String getAnoLetivo() {
+        return anoLetivo;
+    }
+
+    public void setAnoLetivo(String anoLetivo) {
+        this.anoLetivo = anoLetivo;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
     }
 }
